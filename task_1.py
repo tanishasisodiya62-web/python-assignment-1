daf393589d53217990908db631f1f107b7fce68f
tanishasisodiya62-web <tanishasisodiya62@gmail.com>
2026-01-20 17:22:13 +0530

    # Task 1: Basic Mathematical Operations

---
+++ task_1.py
@@ -0,0 +1,17 @@
+# Task 1: Perform Basic Mathematical Operations
+
+# Take two numbers as input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2   # assuming num2 is not zero
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
